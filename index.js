0e2a5d150d56a4ad35668e251f9e521f5df92e30
Radek Novak <radek.novak.cze@gmail.com>
2016-06-09 21:28:46 +0200

    v0.2.0 - use global package folder

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ var fs = require('fs');
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 var serve = serveStatic("./");
+var child_process = require('child_process');
 
-var CERT_DIR = './cert/'
+
+var nodeRootFolder = child_process.execSync(`npm root -g`)
+var certFolder = nodeRootFolder.toString().trim() + '/simple-https-server/cert/'
+
 
 var options = {
-  key: fs.readFileSync(CERT_DIR + 'key.pem'),
-  cert: fs.readFileSync(CERT_DIR + 'cert.pem')
+  key: fs.readFileSync(certFolder + 'key.pem'),
+  cert: fs.readFileSync(certFolder + 'cert.pem')
 };
-
 
 console.log('Starting server on HTTPS://localhost:8000...')
 
